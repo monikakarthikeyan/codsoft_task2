54e259977d5c6eab82a4b8e3ca5df1cb8bf2fcfa
monika sivasamy <monikasivasamy@gmail.com>
2023-08-21 22:59:19 +0530

    codsoft-portfolio first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+function scrollToSkill() {
+    var targetElm = document.querySelector('.pro');
+    targetElm.scrollIntoView();
+}
+function scrollToopensource() {
+    var project = document.querySelector('.project');
+    project.scrollIntoView();
+}
+function scrollTobigproject() {
+    var bigproject = document.querySelector('.big-project');
+    bigproject.scrollIntoView();
+}
+function scrollToachieve() {
+    var achieve = document.querySelector('.achieve');
+    achieve.scrollIntoView();
+}
+function scrollToresume() {
+    var resume = document.querySelector('.resume');
+    resume.scrollIntoView();
+}
+function scrollTocontact() {
+    var contact = document.querySelector('.contact');
+    contact.scrollIntoView();
+}
